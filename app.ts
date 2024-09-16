2c976f4f40da5066c2f11cca533e202ff9e24eff
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:31:34 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,31 @@
+let favourite_fruit: string[] =['apple','orange','banana','mango','grapes']
+if (`${favourite_fruit[0]}`==='apple'){
+    console.log('I like apple')
+}
+else{
+    console.log('I dont like this fruit')
+}
+if (`${favourite_fruit[1]}`==='orange'){
+    console.log('I like  orange')
+}
+else{
+    console.log('I dont like this fruit')
+}
+if (`${favourite_fruit[2]}`==='banana'){
+    console.log('I like banana')
+}
+else{
+    console.log('I dont like this fruit')
+}
+if (`${favourite_fruit[3]}`==='papaya'){
+    console.log('I like papaya')
+}
+else{
+    console.log('I dont like this fruit')
+}
+if (`${favourite_fruit[4]}`==='apple'){
+    console.log('I like apple')
+}
+else{
+    console.log('I dont like this fruit')
+}
